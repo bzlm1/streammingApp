8cd7b63513779490f259f21709831b1a7e011020
Sebastian Ruiz Pulache <s.ruiz.p6@gmail.com>
2025-09-09 15:40:46 -0500

    cambio postBoton

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Pelicula from './Pelicula'
 import PageWrapper from './PageWrapper'
-import PeliculasJson from './peliculas.json'
 import Paginacion from './Paginacion'
 
 function App() {
   // ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
   const itemsXPagina = 7
   const [paginaActual, setPaginaActual] = useState(1)
-  let peliculas = PeliculasJson
-  // const fetchWithProxy = async (url) => {
-  //   try {
-  //     // Usar un servicio de proxy CORS
-  //     const proxyUrl = 'https://corsproxy.io/?';
-  //     const response = await fetch(proxyUrl + encodeURIComponent(url));
+  const [peliculasListas, setPeliculasListas] = useState([])
+  useEffect(() => {
+    buscarPeliculas()
+  }, [])
 
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error! status: ${response.status}`);
-  //     }
-
-  //     const data = await response.json();
-  //     return data;
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //     return null;
-  //   }
-  // };
   const buscarPeliculas = async () => {
     try {
       const url = 'https://api.github.com/repos/bzlm1/streammingApp/contents/src/peliculas.json';
@@ -47,8 +33,9 @@ function App() {
 
       // alert(contenidoDecodificado);
       const peliculas = JSON.parse(contenidoDecodificado);
-      debugger
-      alert(peliculas);
+      setPeliculasListas(peliculas)
+      // 3.15
+      // alert(peliculas);
 
 
 
@@ -62,32 +49,19 @@ function App() {
       alert('Error: ' + error.message);
     }
   }
-  // const buscarPeliculas = async () => {
-  //   const url = 'https://raw.githubusercontent.com/bzlm1/streammingApp/refs/heads/main/src/peliculas.json'
-  //   const respuesta = await fetch(url, {
-  //     "method": 'GET',
-  //     "headers": {
-  //       "Accept": 'application/json',
-  //       "Content-Type": 'application/json'
-  //     }
-  //   })
-  //   const json = await respuesta.json()
-  //   alert(json);
-  //   console.log(json);
 
-  // }
-  buscarPeliculas()
-  const cargarPeliculas = () => {
-    peliculas = peliculas.slice((paginaActual - 1) * itemsXPagina, paginaActual * itemsXPagina)
-  }
   const getTotalPaginas = () => {
-    let cantTotalPeliculas = PeliculasJson.length;
+    let cantTotalPeliculas = peliculasListas.length;
     return Math.ceil(cantTotalPeliculas / itemsXPagina)
   }
-  cargarPeliculas()
+  const peliculasPorPagina =
+    peliculasListas.slice((paginaActual - 1) * itemsXPagina, paginaActual * itemsXPagina)
+
+
   return (
     <PageWrapper>
-      {peliculas.map(pelicula => {
+      <button onClick={buscarPeliculas}>Prueba</button>
+      {peliculasListas.map(pelicula => {
         return <Pelicula
           image={pelicula.image}
           title={pelicula.title}
@@ -101,10 +75,10 @@ function App() {
         />
       })}
       <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
-        {/* <Paginacion pagina={paginaActual} total={Math.ceil(PeliculasJson.length / itemsXPagina)} onChange={(pagina) => { */ }
+
         setPaginaActual(pagina)
       }} />
-      {/* <Paginacion /> */}
+
     </PageWrapper>
 
 
