086f0fff925d7af3951f13860c8657b8c04f19a1
Sebastian Ruiz Pulache <s.ruiz.p6@gmail.com>
2025-09-10 13:19:33 -0500

    react router-dom 7.8.2, no me sale :(

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,14 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Pelicula from './Pelicula'
-import PageWrapper from './PageWrapper'
-import Paginacion from './Paginacion'
+import ListadoPeliculas from './ListadoPeliculas'
+import Blog from './Blog'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  // ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  const itemsXPagina = 3
-  const [paginaActual, setPaginaActual] = useState(1)
-  const [peliculasListas, setPeliculasListas] = useState([])
-  useEffect(() => {
-    buscarPeliculas()
-  }, [])
-
-  const buscarPeliculas = async () => {
-    try {
-      const url = 'https://api.github.com/repos/bzlm1/streammingApp/contents/src/peliculas.json';
-
-      // 1. Fetch MÁS SIMPLE - sin headers complicados
-      const respuesta = await fetch(url);
-
-      if (!respuesta.ok) {
-        throw new Error(`Error ${respuesta.status}: ${respuesta.statusText}`);
-      }
-
-      // 2. GitHub devuelve un objeto con el contenido en base64
-      const data = await respuesta.json();
-      // 3. ¡ESTE ES EL PASO NUEVO! Decodificar el base64
-      const contenidoDecodificado = atob(data.content); // ← Esto es lo diferente
-
-      // alert(contenidoDecodificado);
-      const peliculas = JSON.parse(contenidoDecodificado);
-      setPeliculasListas(peliculas)
-      // 3.15
-      // alert(peliculas);
-
-
-
-      // 4. Ahora sí tienes tus películas
-      // console.log(peliculas);
-      // alert(`Se cargaron ${peliculas.length} películas`);
-      return peliculas;
-
-    } catch (error) {
-      console.error('Error:', error);
-      alert('Error: ' + error.message);
-    }
-  }
-
-  const getTotalPaginas = () => {
-    let cantTotalPeliculas = peliculasListas.length;
-    return Math.ceil(cantTotalPeliculas / itemsXPagina)
-  }
-  const peliculasPorPagina =
-    peliculasListas.slice((paginaActual - 1) * itemsXPagina, paginaActual * itemsXPagina)
-
-
   return (
-    <PageWrapper>
-
-      {peliculasPorPagina.map(pelicula => {
-        return <Pelicula
-          image={pelicula.image}
-          title={pelicula.title}
-          anho={pelicula.anho}
-          descripcion={pelicula.descripcion}
-          duracion={pelicula.duracion}
-          mmpa={pelicula.mmpa}
-          fechaDeEstreno={pelicula.fechaDeEstreno}
-          director={pelicula.director}
-          estrellas={pelicula.estrellas}
-        />
-      })}
-      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
-
-        setPaginaActual(pagina)
-      }} />
-
-    </PageWrapper>
-
-
+    <Routes>
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/" element={<ListadoPeliculas />} />
+    </Routes>
   )
 }
 
---
+++ src/Blog.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Pelicula from './Pelicula'
+import PageWrapper from './PageWrapper'
+import Paginacion from './Paginacion'
+
+function Blog() {
+  const itemsXPagina = 3
+  const [paginaActual, setPaginaActual] = useState(1)
+  const [peliculasListas, setPeliculasListas] = useState([])
+  useEffect(() => {
+    buscarPeliculas()
+  }, [])
+
+  const buscarPeliculas = async () => {
+    try {
+      const url = 'https://api.github.com/repos/bzlm1/streammingApp/contents/src/peliculas.json';
+
+      // 1. Fetch MÁS SIMPLE - sin headers complicados
+      const respuesta = await fetch(url);
+
+      if (!respuesta.ok) {
+        throw new Error(`Error ${respuesta.status}: ${respuesta.statusText}`);
+      }
+
+      // 2. GitHub devuelve un objeto con el contenido en base64
+      const data = await respuesta.json();
+      // 3. ¡ESTE ES EL PASO NUEVO! Decodificar el base64
+      const contenidoDecodificado = atob(data.content); // ← Esto es lo diferente
+
+      // alert(contenidoDecodificado);
+      const peliculas = JSON.parse(contenidoDecodificado);
+      setPeliculasListas(peliculas)
+      // 3.15
+      // alert(peliculas);
+
+
+
+      // 4. Ahora sí tienes tus películas
+      // console.log(peliculas);
+      // alert(`Se cargaron ${peliculas.length} películas`);
+      return peliculas;
+
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Error: ' + error.message);
+    }
+  }
+
+  const getTotalPaginas = () => {
+    let cantTotalPeliculas = peliculasListas.length;
+    return Math.ceil(cantTotalPeliculas / itemsXPagina)
+  }
+  const peliculasPorPagina =
+    peliculasListas.slice((paginaActual - 1) * itemsXPagina, paginaActual * itemsXPagina)
+
+
+  return (
+    <PageWrapper>
+      {/* En esta pagina mostramos un listado de peliculas */}
+      {peliculasPorPagina.map(pelicula => {
+        return <Pelicula
+          image={pelicula.image}
+          title={pelicula.title}
+          anho={pelicula.anho}
+          descripcion={pelicula.descripcion}
+          duracion={pelicula.duracion}
+          mmpa={pelicula.mmpa}
+          fechaDeEstreno={pelicula.fechaDeEstreno}
+          director={pelicula.director}
+          estrellas={pelicula.estrellas}
+        />
+      })}
+      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
+
+        setPaginaActual(pagina)
+      }} />
+
+    </PageWrapper>
+
+
+  )
+}
+
+
+
+export default Blog
