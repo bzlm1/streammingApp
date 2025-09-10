ed296c7c55dc74a05dffd12ed77cd76f539aaf68
Sebastian Ruiz Pulache <s.ruiz.p6@gmail.com>
2025-09-10 17:00:41 -0500

    react router-dom 7.8.2, no me sale x2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react'
 import ListadoPeliculas from './ListadoPeliculas'
 import Blog from './Blog'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/" element={<ListadoPeliculas />} />
-    </Routes>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/">
+          <ListadoPeliculas />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   )
 }
 
